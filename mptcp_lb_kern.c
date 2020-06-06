613f123d0b77b992bb1ae529ff181d2f33bffa9d
Takanori Hirano <hrn.takanori@gmail.com>
2020-06-07 00:30:55 +0900

    remove bpf_printk

--- mptcp_lb_kern.c
+++ mptcp_lb_kern.c
@@ -17,13 +17,6 @@
     if ((void *)(target + 1) > end) \
         return XDP_DROP;
 
-#define bpf_printk(fmt, ...)                       \
-    ({                                             \
-        char ____fmt[] = fmt;                      \
-        bpf_trace_printk(____fmt, sizeof(____fmt), \
-                         ##__VA_ARGS__);           \
-    })
-
 struct services_key
 {
     __u8 addr[16];
